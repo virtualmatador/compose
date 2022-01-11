49b6fdfd3e4ac5aaae8da0c785ac7edb5a0c13cc
Ali Asadpoor <virtualmatador@yahoo.com>
2022-01-10 20:30:07 -0500

    Implement compose

---
+++ src/compose.h
@@ -0,0 +1,48 @@
+#ifndef COMPOSE_H
+#define COMPOSE_H
+
+#include <atomic>
+#include <chrono>
+#include <filesystem>
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <json.h>
+
+class compose
+{
+public:
+    compose(const std::filesystem::path& pipe_path,
+        const std::chrono::milliseconds& nap_time);
+    ~compose();
+    void run();
+    void add_reloader(const std::shared_ptr<std::function<void()>>& reloader);
+    void add_ticker(const std::shared_ptr<std::function<void()>>& ticker,
+        const std::chrono::milliseconds& interval);
+    void add_handler(const std::string& command, const std::shared_ptr<
+        std::function<void(const jsonio::json&)>>& handler);
+    bool need_reload() const;
+    bool need_stop() const;
+    void request_reload();
+    void request_stop();
+private:
+    std::filesystem::path pipe_path_;
+    const std::chrono::milliseconds nap_time_;
+    std::vector<std::weak_ptr<std::function<void()>>> reloaders_;
+    std::vector<std::pair<std::weak_ptr<std::function<void()>>,
+        std::array<std::chrono::milliseconds, 2>>> tickers_;
+    std::map<std::string, std::vector<std::weak_ptr<
+        std::function<void(const jsonio::json&)>>>> handlers_;
+
+private:
+    static std::atomic<bool> reload_;
+    static std::atomic<bool> stop_;
+
+private:
+    static void handle_signal(int signal);
+};
+
+#endif //COMPOSE_H
---
+++ test/basic.cpp
@@ -0,0 +1,144 @@
+#include <atomic>
+#include <chrono>
+#include <csignal>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <regex>
+#include <sstream>
+
+#include <compose.h>
+
+bool t01()
+{
+    std::size_t reloads = 0;
+    compose the_compose { "", std::chrono::seconds(0) };
+    the_compose.request_reload();
+    auto reloader = std::make_shared<std::function<void()>>([&]()
+    {
+        reloads += 1;
+        the_compose.request_stop();
+    });
+    the_compose.add_reloader(reloader);
+    the_compose.run();
+    if (reloads != 1)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool t02()
+{
+    std::size_t reloads = 0;
+    compose the_compose("", std::chrono::seconds(0));
+    auto ticker = std::make_shared<std::function<void()>>([&]()
+    {
+        reloads += 2;
+        the_compose.request_reload();
+        if (reloads > 5)
+        {
+            the_compose.request_stop();
+        }
+    });
+    auto reloader = std::make_shared<std::function<void()>>([&]()
+    {
+        reloads += 1;
+    });
+    the_compose.add_reloader(reloader);
+    the_compose.add_ticker(ticker, std::chrono::milliseconds(0));
+    the_compose.run();
+    if (reloads != 8)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool t03()
+{
+    std::size_t handles = 0;
+    std::size_t ticks = 0;
+    compose the_compose("./test.pipe", std::chrono::seconds(0));
+    auto ticker = std::make_shared<std::function<void()>>([&]()
+    {
+        if (ticks++ == 0)
+        {
+            std::ofstream pipe{"./test.pipe"};
+            pipe << "{\"command\": \"test\", \"payload\": "
+                "{\"data\": \"some data\"}}" << std::endl;
+        }
+    });
+    auto handler = std::make_shared<std::function<void(const jsonio::json&)>>(
+        [&](auto payload)
+    {
+        if (payload["data"].get_string() == "some data")
+        {
+            handles += 1;
+        }
+        the_compose.request_stop();
+    });
+    the_compose.add_handler("test", handler);
+    the_compose.add_ticker(ticker, std::chrono::milliseconds(0));
+    the_compose.run();
+    if (handles != 1)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool t04()
+{
+    std::size_t handles = 0;
+    std::size_t ticks = 0;
+    compose the_compose("./test.pipe", std::chrono::milliseconds(1));
+    auto ticker = std::make_shared<std::function<void()>>([&]()
+    {
+        if (ticks++ == 0)
+        {
+            the_compose.request_reload();
+        }
+    });
+    auto reloader = std::make_shared<std::function<void()>>([&]()
+    {
+        std::ofstream pipe{"./test.pipe"};
+        if (pipe.is_open())
+        {
+            pipe << "{\"command\": \"test\", \"payload\": "
+                "{\"data\": \"reload\"}}" << std::endl;
+        }
+    });
+    auto handler = std::make_shared<std::function<void(const jsonio::json&)>>(
+        [&](auto payload)
+    {
+        if (payload["data"].get_string() == "reload")
+        {
+            handles += 1;
+            the_compose.request_stop();
+        }
+    });
+    the_compose.add_handler("test", handler);
+    the_compose.add_ticker(ticker, std::chrono::milliseconds(10));
+    the_compose.add_reloader(reloader);
+    the_compose.run();
+    if (handles != 1)
+    {
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    if (
+        t01() &&
+        t02() &&
+        t03() &&
+        t04() &&
+        true)
+    {
+        return 0;
+    }
+    return -1;
+}
